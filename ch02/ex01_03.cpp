7cc2d8e450e08f3ded2552bf0eb3cac11fda95b1
Rubin <1341917538@qq.com>
2019-11-17 17:20:46 +0800

    Create ex01_03.cpp

---
+++ ch02/ex01_03.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int first()
+{
+	cout << "Three blind mice" << endl;
+	return 0;
+}
+int second()
+{
+	cout << "See how they run" << endl;
+	return 0;
+}
+int main()
+{
+	first();
+	first();
+	second();
+	second();
+	return 0;
+}
